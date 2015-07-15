6c17c525e61ef5c4c9b3b3418bc8f86ad06e7d99
Heather <hdavidson94@gmail.com>
2015-07-15 22:27:45 +0100

    Favicon Fix

--- header.php
+++ header.php
@@ -11,12 +11,13 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <script src="//use.typekit.net/xuo7gug.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
+<link rel="icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" type="image/x-icon" />
 
 <?php wp_head(); ?>
 </head>
