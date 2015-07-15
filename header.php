40ff04b047d65c668c1ea6efd7ca2296d8a3604c
Heather <hdavidson94@gmail.com>
2015-07-15 22:22:10 +0100

    Favicon update

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
